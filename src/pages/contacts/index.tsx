4582d60423a2014e1f3733848fef65cbe1017420
Ismael-Oliv <ismaelalves1@outlook.com.br>
2021-08-05 18:43:14 -0300

    Template: Add Contact me

--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -1,7 +1,30 @@
+import styles from "./styles.module.scss";
+
 export default function Contacts() {
   return (
     <main>
-      <h1>Contacts Page</h1>
+      <section className={styles.content}>
+        <form action="">
+          <h1>Enviar mensagem</h1>
+          <div className={styles.title}>
+            <h2>Titulo:</h2>
+            <input type="text" name="title" />
+          </div>
+
+          <div className={styles.email}>
+            <h2>Email:</h2>
+            <input type="email" name="email" />
+          </div>
+
+          <div className={styles.mensagem}>
+            <h2>Mensagem:</h2>
+            <textarea name="mensagem" cols={1} rows={10} />
+          </div>
+          <div className={styles.button}>
+            <button type="submit">Enviar</button>
+          </div>
+        </form>
+      </section>
     </main>
   );
 }
